a67e82f8f62bd497b67fbf1b67b14e49ed253e04
Saifurrehmanbaloch <surrehman180@gmail.com>
2022-02-14 12:43:44 +0500

    add the files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React, {useEffect, useRef} from 'react';
+import './App.css';
+import useWebAnimations from '@wellyshen/use-web-animations';
 
 function App() {
-  const element = useRef(null);
+  // const element = useRef(null);
 
-  useEffect(() => {
-    element.current.focus();
-  })
+  
+  const { ref, playState, getAnimation } = useWebAnimations({
+    keyframes: [{ transform: 'translate(0,0)' }, { transform: 'translate(600px,0)' }],
+    animationOptions: {
+      // delay: 500, // Start with a 500ms delay
+      duration: 1000, // Run for 1000ms
+      iterations: Infinity, // Repeat once
+      direction: "alternate", // Run the animation forwards and then backwards
+      easing: "ease-in-out", // Use a fancy timing function
+    },
+    // onReady: ({ playState, animate, animation }) => {
+    //   // Triggered when the animation is ready to play
+    // },
+    // onUpdate: ({ playState, animate, animation }) => {
+    //   // Triggered when the animation enters the running state or changes state
+    // },
+    // onFinish: ({ playState, animate, animation }) => {
+    //   // Triggered when the animation enters the finished state
+    // },
+    // // More useful options...
+  });
   return (
     <div>
-      <input ref={element} type="text"></input> 
+      <div className='target' ref={ref}></div>
+      Current animation state: {playState}
+      <button onClick={() => getAnimation().pause()}>Pause</button>
+      <button onClick={() => getAnimation().play()}>Play</button>
     </div>
   );
 }
